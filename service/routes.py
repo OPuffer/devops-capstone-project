8b019a62fc036ad00cf4d4e14fa114c932dfc4a7
OPuffer <pufferoliver@gmail.com>
2023-08-26 16:04:21 -0700

    implemented account deletion

--- service/routes.py
+++ service/routes.py
@@ -93,6 +93,13 @@ def read_account(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    account.delete()
+    return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
